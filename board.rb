607754c7b0e1cd404bbc45c74c3215d9062fe803
Matt Kline <mattkline13@gmail.com>
2016-12-03 17:29:44 -0700

    Original Template

---
+++ board.rb
@@ -0,0 +1,42 @@
+class Board
+
+  BOARD_MAX_INDEX = 2
+  EMPTY_POS = ' '
+
+  def initialize(starting_player)
+    # implement
+  end
+
+  def display
+    # implement
+  end
+
+  def board_full
+    # implement
+  end
+
+  def winner
+    # implement
+  end
+
+  def winner_cols
+    # implement
+  end
+
+  def winner_rows
+    # implement
+  end
+
+  def winner_diagonals
+    # implement
+  end
+
+  def validate_position(row, col)
+    # implement
+  end
+
+  def fill_position(row, col, player)
+    # implement
+  end
+
+end
---
+++ tic_tac_toe.rb
@@ -0,0 +1,4 @@
+require_relative 'game.rb'
+
+ game = Game.new('X')
+ game.play
