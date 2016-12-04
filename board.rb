47e7c329db0a8054c93f8d1f564cb7a6140e2111
Matt Kline <mattkline13@gmail.com>
2016-12-03 19:01:57 -0700

    Add files via upload

--- board.rb
+++ board.rb
@@ -4,8 +4,12 @@ class Board
   EMPTY_POSITION = ' '
 
   def initialize(starting_player)
-    @current_player = current_player
+    @current_player = starting_player
     @board = Array.new(BOARD_MAX_INDEX + 1) {Array.new(BOARD_MAX_INDEX + 1, EMPTY_POSITION)}
+  end
+
+  def size
+    BOARD_MAX_INDEX + 1
   end
 
   def display
@@ -35,44 +39,62 @@ class Board
   end
 
   def winner
-    if winner == winner_columns || winner == winner_rows || winner == winner_diagonals
+    winner = winner_rows
+    if winner != nil
+      return winner
+    else
+      winner = winner_columns
+      if winner != nil
         return winner
-    end
-  end
-
-  def winner_columns
-    for column in 0..BOARD_MAX_INDEX
-      to_find = @board[0][column]
-      if to_find == EMPTY_POSITION
-        next
-      end
-      for row in 0..BOARD_MAX_INDEX
-        if to_find != @board[row][column]
-          break
-        elsif row == BOARD_MAX_INDEX
-          return to_find
+      else
+        winner = winner_diagonals
+        if winner != nil
+          return winner
+        else
+          return false
         end
       end
     end
   end
 
   def winner_rows
+    puts 'rows win check'
     for row in 0..BOARD_MAX_INDEX
       to_find = @board[row][0]
       if to_find == EMPTY_POSITION
         next
       end
-      for column in 0..BOARD_MAX_INDEX
+      for column in 1..BOARD_MAX_INDEX
         if to_find != @board[row][column]
-          break
-        elsif column == BOARD_MAX_INDEX
+          return nil
+        elsif column == BOARD_MAX_INDEX && to_find != EMPTY_POSITION
           return to_find
         end
       end
     end
+    return nil
+  end
+
+  def winner_columns
+    puts 'columns win check'
+    for column in 0..BOARD_MAX_INDEX
+      to_find = @board[0][column]
+      if to_find == EMPTY_POSITION
+        next
+      end
+      for row in 1..BOARD_MAX_INDEX
+        if to_find != @board[row][column]
+          return nil
+        elsif row == BOARD_MAX_INDEX && to_find != EMPTY_POSITION
+          return to_find
+        end
+      end
+    end
+    return nil
   end
 
   def winner_diagonals
+    puts 'diagonals win check'
     for index in 0..BOARD_MAX_INDEX
       to_find = @board[0][0]
       if to_find == EMPTY_POSITION
@@ -80,32 +102,35 @@ class Board
       end
         if to_find != @board[index][index]
           break
-        elsif index == BOARD_MAX_INDEX
+        elsif index == BOARD_MAX_INDEX && to_find != EMPTY_POSITION
           return to_find
         end
     end
 
-    for index in 0..BOARD_MAX_INDEX
-      to_find = @board[0][3]
+    for index2 in 0..BOARD_MAX_INDEX
+      to_find = @board[0][2]
       if to_find == EMPTY_POSITION
         break
       end
-      if to_find != @board[index][(BOARD_MAX_INDEX + 1) - index]
-        break
-      elsif index == BOARD_MAX_INDEX
+      if to_find != @board[index2][BOARD_MAX_INDEX - index2]
+        return nil
+      elsif index2 == BOARD_MAX_INDEX
         return to_find
       end
     end
+    return nil
   end
 
   def validate_position(row, column)
-    if row <= BOARD_MAX_INDEX && column <= BOARD_MAX_INDEX
+    if row >= 0 && row <= BOARD_MAX_INDEX && column >= 0 && column <= BOARD_MAX_INDEX
       if @board[row][column] == EMPTY_POSITION
         return true
       else
-        return false
+        puts 'Position occupied'
       end
-    end
+    else
+      puts ' Invalid position'
+      end
   end
 
   def fill_position(row, column, player)
